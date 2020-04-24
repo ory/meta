5b78095418652ed0e9e422c5fd94cad9a2b2bb01
aeneasr <aeneas@ory.sh>
2020-04-24 12:49:25 +0200

    feat: use minor, patch, major as args

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-set -Eeuox pipefail
+set -Eeuo pipefail
 
-if echo "$1" | grep -Eq "^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?$"; then
-  echo "$1 is a valid release tag!"
-else
-  echo "$1 is not a valid semver!"
+if ! (echo "$1" | grep -Eq "^(major|minor|patch)$"); then
+  echo "$1 is not valid, choose major, minor, or patch!"
   exit 1
+fi
+
+go get github.com/davidrjonas/semver-cli
+go install github.com/davidrjonas/semver-cli
+
+prev=$(git describe --abbrev=0)
+bumpTo=$(semver-cli inc "$1" "$prev")
+
+if grep -q "$bumpTo" <(git tag); then
+  echo "There was a problem. It appears that tag $bumpTo already exists!"
+  exit 1
+fi
+
+read -p "Are you sure you want to bump to $bumpTo? Previous version was $prev. [yN] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
 fi
 
 git checkout master
@@ -16,7 +32,9 @@ git diff --exit-code
 goreleaser check
 circleci config check
 
-git commit --allow-empty -m "chore: pin $1 release commit"
-git tag -a "$1"
+git commit --allow-empty -m "chore: pin $bumpTo release commit"
+git tag -a "$bumpTo"
 git push
 git push --tags
+
+echo "Successfully released $bumpTo"
