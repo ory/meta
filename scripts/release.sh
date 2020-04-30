81d4c7fc15355c2b592d3964efb5cd0f7605b2e1
aeneasr <aeneas@ory.sh>
2020-04-30 12:18:32 +0200

    fix: resolve manual override issues

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,20 +4,20 @@ set -Eeuo pipefail
 
 if (echo "$1" | grep -Eq "^(major|minor|patch)$"); then
   echo "$1 is a valid release tag!"
+  go get github.com/davidrjonas/semver-cli@1.1.0
+  go install github.com/davidrjonas/semver-cli
+  git checkout -- go.mod
+  git checkout -- go.sum
+  bumpTo=v$(semver-cli inc "$1" "$prev")
 elif echo "$1" | grep -Eq "^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?$"; then
   echo "$1 is a valid release tag!"
+  bumpTo="$1"
 else
   echo "$1 is not valid, choose 'major', 'minor', 'patch' or a valid semantic version (v1.2.3-alpha.123)!"
   exit 1
 fi
 
-go get github.com/davidrjonas/semver-cli@1.1.0
-go install github.com/davidrjonas/semver-cli
-git checkout -- go.mod
-git checkout -- go.sum
-
-prev=$(git describe --abbrev=0)
-bumpTo=v$(semver-cli inc "$1" "$prev")
+prev=$(git describe --abbrev=0 --tags)
 
 if grep -q "$bumpTo" <(git tag); then
   echo "There was a problem. It appears that tag $bumpTo already exists!"
