fce2b7800d64a0087dbde6e4dd8dabf43fc35902
aeneasr <aeneas@ory.sh>
2020-06-08 18:22:20 +0200

    fix: remove spf13/viper

--- tools/ory-dev/cmd/root.go
+++ tools/ory-dev/cmd/root.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -21,7 +6,7 @@ import (
 	"os"
 
 	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
+	"github.com/ory/viper"
 )
 
 var cfgFile string
@@ -29,16 +14,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ory-dev",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "ORY development toolchain",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
--- tools/ory-dev/go.mod
+++ tools/ory-dev/go.mod
@@ -5,11 +5,12 @@ go 1.14
 require (
 	github.com/google/uuid v1.1.0
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/ory/viper v1.7.5
 	github.com/ory/x v0.0.128
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.0.0
-	github.com/spf13/viper v1.7.0
 	github.com/stretchr/testify v1.5.1
 	github.com/tidwall/gjson v1.3.2
 	github.com/tidwall/sjson v1.0.4
+	google.golang.org/genproto v0.0.0-20191108220845-16a3f7862a1a // indirect
 )
