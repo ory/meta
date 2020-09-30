c2e065067e4334b8a2fb29038cc5ea846fc5fc79
zepatrik <patrik@ory.sh>
2020-09-30 23:02:58 +0200

    feat: add action repo template

---
+++ scripts/sync-action.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -Eexuo pipefail
+
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+source scripts/sync.sh
+
+workdir=$(mktemp -d)
+project=$1
+branch=$2
+humanName=$3
+
+git clone --depth 1 --branch "$branch" "git@github.com:$1.git" "$workdir"
+sync "$workdir" "$branch" "action" "$humanName" "$project"
