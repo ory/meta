d3a2438027877f14e8433bb44431544b9b91d210
Vincent <vincent@ory.sh>
2021-01-05 13:22:01 +0100

    docs: add codedoc for shell scripts  (#75)
    Closes #74

--- README.md
+++ README.md
@@ -19,3 +19,18 @@ files from [server](./templates/repository/server) and libraries from the
 
 To update the repositories simply make your changes. Once merged to master, they
 will be published using a GitHub Action.
+
+## Github Sync action
+
+The [meta scripts](https://github.com/ory/meta/tree/master/scripts) serve to
+synchronize all ORY repositories to a common template, including README,
+CONTRIBUTING, COC, SECURITY, LICENCE and Github Workflows with close to zero
+manual interaction.  
+Depending on repository type (server, library, action) specific templates can be
+copied as well.  
+The project names, links to documentation ect. are being substituted for each
+project in [sync.sh](https://github.com/ory/meta/blob/master/scripts/sync.sh).  
+For more details please refer to the documentation within the
+[scripts](https://github.com/ory/meta/tree/master/scripts). For more details on
+the workflow please refer to the documentation within
+[sync.yml](https://github.com/ory/meta/blob/master/.github/workflows/sync.yml)
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+# Causes the script to abort on error
 set -Eexuo pipefail
 
+# Set bin to temporary directory named bin-XXXXXX), set path to bin
 bin=$(mktemp -d -t bin-XXXXXX)
 export PATH="$PATH:$bin"
 
+# Set git email & username as specified in git.sh, change global url from https://github.com/ to git@github.com:
 bash <(curl -s https://raw.githubusercontent.com/ory/ci/master/src/scripts/install/git.sh)
 git config --global url."git@github.com:".insteadOf https://github.com/
 
 function sync {
+
+    # Change directory to the path of the invoking script, set variables workdir, branch, type, humanName, project, set upstream branch as pushBranch, 
     cd "$( dirname "${BASH_SOURCE[0]}" )/.."
     workdir=$1
     branch=$2
     type=$3
     humanName=$4
     project=$5
+    # discussions="https://github.com/${REPOSITORY}/discussions"
     pushBranch="meta-$(date +%m-%d-%y-%H-%M-%S)"
+    # set hash as github commit hash, first 8 bytes
     hash=$(echo $GITHUB_SHA | head -c 8)
 
+    # in subshell, change directory to workdir, checkout git branch specified in pushBranch
     (cd "$workdir"; git checkout -b "$pushBranch")
 
-    # Copy common templates
+    # Copy common templates to workdir
     cp -R "templates/repository/common/CONTRIBUTING.md" "$workdir/CONTRIBUTING.md"
     cp -R "templates/repository/common/SECURITY.md" "$workdir/SECURITY.md"
     cp -R "templates/repository/common/LICENSE" "$workdir/LICENSE"
@@ -28,11 +36,15 @@ function sync {
     cp -n "templates/repository/common/.reference-ignore" "$workdir/.reference-ignore" || true
     cp -R "templates/repository/common/.github" "$workdir/"
 
-    # Copy specific templates for servers or library
+    # Copy specific templates for servers or library (depending on $type)
     cp -R "templates/repository/$type/.github" "$workdir/"
 
-    for f in $(find "$workdir/.github/ISSUE_TEMPLATE" "$workdir/.github/pull_request_template.md" "$workdir/CONTRIBUTING.md" "$workdir/SECURITY.md" "$workdir/CODE_OF_CONDUCT.md" "$workdir/LICENSE" -type f -print); do
-      env -i REPOSITORY="$project" PROJECT="$humanName" /bin/bash -c "envsubst < \"$f\" | sponge \"$f\""
+    # Replace placeholders - find in regular files (type f)
+    # env clears environment and sets REPOSITORY and PROJECT as new env variables
+    # /bin/bash -c execute commands in " " in the new environment 
+    # envsubst: replace all env variables found ($f) with the ones specified 2 steps before, pipe to sponge and write to file
+    for f in $(find "./.github/ISSUE_TEMPLATE" " ./.github/pull_request_template.md" "CONTRIBUTING.md" "SECURITY.md" "CODE_OF_CONDUCT.md" "LICENSE" -type f -print); do
+        env -i REPOSITORY="$project" PROJECT="$humanName" /bin/bash -c "envsubst < \"$f\" | sponge \"$f\""
     done
 
     # Copy contributing guide to docs if docs exist
@@ -53,9 +65,13 @@ EOF
         )
     fi
 
+    # Add Adopters to README.md 
     perl -0pe 's#<!--\s*BEGIN ADOPTERS\s*-->.*<!--\s*END ADOPTERS\s*-->\n#`cat templates/repository/common/ADOPTERS.md`#gse' -i "$workdir/README.md"
+    # Add Ecosystem (overview of all projects) to README.md
     perl -0pe 's#<!--\s*BEGIN ECOSYSTEM\s*-->.*<!--\s*END ECOSYSTEM\s*-->\n#`cat templates/repository/common/PROJECTS.md`#gse' -i "$workdir/README.md"
 
+    # Commit changes - line by line: change to workdir, add all files to git index, show working tree status, commit changes with "chore:..." title, push to upstream 
+    # curl: make pull request through Github API with token to authenticate and the PR titles, body. With pushBranch (the "script" branch) as head and the specified branch (main) as base
     (cd "$workdir"; \
       git add -A; \
       git status; \
